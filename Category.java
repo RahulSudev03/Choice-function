6b8018192b346e17f3229a4c80703f7f29934482
Rahul Sudev <rahul03@iastate.edu>
2024-04-21 20:12:16 -0500

    Set up initial stuff

---
+++ Category.java
@@ -0,0 +1,3 @@
+public enum Category {
+    OPEN,SC,ST,OBC,EWS
+}
---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student {
+
+    private Gender gender;
+    private Category category;
+    private int ranking;
+
+    public Student(Gender gender, Category category, int ranking){
+        this.gender = gender;
+        this.category = category;
+        this.ranking = ranking;
+    }
+}
