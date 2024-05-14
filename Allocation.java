84fd2d2e9c306f7b561f148a11f18b3f54dd2678
saeshukarthika <saeshukarthika@gmail.com>
2024-05-14 17:32:45 -0500

    added logic

--- Allocation.java
+++ Allocation.java
@@ -1,4 +1,16 @@
 public class Allocation {
     
+//sort students based on ranking
+//set the number of seats for each reservatoin categories
+//have one function that updates the reservation seats - call this function for each category
 
+/**
+ * function(Category, Women seats, total seats, Remaining students) -  output List of Students allocated in the category
+ * - we allocate seats for women
+ * if there are more women than allocated women seats --> women seats = top ranked women in that category 
+ *                                                    --> total seats - women seats = top ranked students(everyone) in that category
+ * else if there are less women than allocated women seats --> fill women seats = top ranked women in that category
+ *                                                         --> total seats - filled women seats = top ranked students in that category
+ * 
+ */
 }
